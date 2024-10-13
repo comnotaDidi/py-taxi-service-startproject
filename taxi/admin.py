6e4dac180181c635230365e88253e450356e1732
anton <anton@gmail.com>
2024-10-13 14:20:06 +0200

    Solut

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,28 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-
-from .models import Manufacturer, Driver, Car
-
-
-@admin.register(Manufacturer)
-class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ("name", "country")
-
-
-@admin.register(Driver)
-class DriverAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ("Additional info", {"fields": ("license_number",)}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Additional info", {"fields": ("license_number",)}),
-    )
-    list_display = UserAdmin.list_display + ("license_number",)
+from taxi.models import Car, Driver, Manufacturer
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ("model", "manufacturer")
-    list_filter = ("manufacturer",)
-    search_fields = ("model",)
+    search_fields = ['model']
+    list_filter = ['manufacturer']
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('license_number', )
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional info', {'fields': ('license_number',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional info', {'fields': ('license_number', )}),
+    )
+
+
+admin.site.register(Manufacturer)
--- taxi/models.py
+++ taxi/models.py
@@ -1,31 +1,37 @@
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
-
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return f"{self.name} {self.country}"
 
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=20, unique=True)
-
-    def __str__(self):
-        return self.username
+    license_number = models.CharField(max_length=63, unique=True)
 
     class Meta:
-        permissions = AbstractUser._meta.permissions
+        ordering = ('username', )
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
 
 
 class Car(models.Model):
     model = models.CharField(max_length=255)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(Driver, blank=True)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name='cars'
+    )
+    drivers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='cars'
+    )
 
-
-    def __str__(self):
-        return self.model
+    def __str__(self) -> str:
+        return f"{self.model} {self.manufacturer}"
